243a436e0d114c97de6ba0fad3a393afc13dff52
Max <maxabouchar@gmail.com>
2024-04-10 17:18:38 -0700

    animated alpha symbol

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import { Container, Typography, Button } from '@mui/material';
+import AnimatedAlpha from './AnimatedAlphaLower';
 
 function HeroSection() {
     const theme = useTheme();
     
     return (
-        <Container maxWidth="xl" style={{ padding: '40px 0', textAlign: 'center' }}>
-        <Typography variant="h2" component="h1" style={{ color: theme.palette.text.primary }}gutterBottom>
-            Welcome to AlphaCharter
-        </Typography>
-        <Typography variant="h5" component="p" gutterBottom>
-            Your on-demand junior quant researcher.
-        </Typography>
-        <Button variant="contained" color="primary" href="/login">
-            Log In / Sign Up
-        </Button>
+        <Container maxWidth="xl" style={{ padding: '40px 0', textAlign: 'center', position: 'relative' }}>
+            <AnimatedAlpha style={{ position: 'absolute', top: '10px', left: '10px', zIndex: 0 }} />
+            <Typography variant="h2" component="h1" style={{ color: theme.palette.text.primary }}gutterBottom>
+                Welcome to AlphaCharter
+            </Typography>
+            <Typography variant="h5" component="p" gutterBottom>
+                Your on-demand quantitative researcher.
+            </Typography>
+            <Button variant="contained" color="primary" href="/login">
+                Log In / Sign Up
+            </Button>
         </Container>
     );
 }
