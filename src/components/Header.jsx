2a2e4addb1af1c17ecc49ad56a2c2010c1de275a
Max <maxabouchar@gmail.com>
2024-04-17 15:28:09 -0700

    rename to AlphaAI and add buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,21 @@ import { useTheme } from '@mui/material/styles';
 function Header() {
     const theme = useTheme();
 
+    const chatButtonStyle = {
+        backgroundColor: theme.palette.secondary.main,
+        color: theme.palette.secondary.contrastText,
+        margin: '0 12px',
+        borderRadius: theme.shape.borderRadius,
+        padding: '6px 16px',
+        textTransform: 'none',
+        fontWeight: 'bold',
+        fontSize: 16,
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.dark,
+            boxShadow: '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)'
+        }
+    };
+
     return (
         <AppBar position='static'>
             <Toolbar style={{backgroundColor: theme.palette.background.header, color: theme.palette.text.header}}>
@@ -13,7 +28,7 @@ function Header() {
                     <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>AlphaCharter</Link>
                 </Typography>
                 <Button component={Link} to="/" color="inherit">Home</Button>
-                <Button component={Link} to="/chat" color="inherit">Chat</Button>
+                <Button component={Link} to="/chat" sx={chatButtonStyle}>AlphaAI</Button>
                 {/* <Button component={Link} to="/about" color="inherit">About</Button> */}
                 <Button component={Link} to="/login" color="inherit">Login</Button>
             </Toolbar>
--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -12,11 +12,14 @@ function HeroSection() {
             <Typography variant="h2" component="h1" style={{ color: theme.palette.text.primary }}gutterBottom>
                 Welcome to AlphaCharter
             </Typography>
-            <Typography variant="h5" component="p" gutterBottom>
+            <Typography variant="h5" component="p" gutterBottom sx={{ margin: 5}}>
                 Your on-demand quantitative researcher.
             </Typography>
             <Button variant="contained" color="primary" href="/login">
                 Log In / Sign Up
+            </Button>
+            <Button variant='contained' color='secondary' sx={{ textTransform:'none', marginLeft: 5 }} href='/chat'>
+                GO TO &nbsp; <strong>AlphaAI</strong>
             </Button>
         </Container>
     );
