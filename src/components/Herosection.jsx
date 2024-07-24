5374b1e687343a53742e45523cb62eea3777f9a4
Aria Han <ariaxhan@gmail.com>
2024-07-23 17:05:40 -0700

    button changed

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { Container, Typography, Button } from '@mui/material';
-import AnimatedAlpha from './AnimatedAlphaLower';
-import { trackEvent } from '../analytics';
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import { Container, Typography, Button } from "@mui/material";
+import AnimatedAlpha from "./AnimatedAlphaLower";
+import { trackEvent } from "../analytics";
 
 function HeroSection() {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const handleLoginClick = () => {
-        trackEvent('Button', 'Click', 'Hero Login Button Click');
-        // Navigate to the href
-        window.location.href = '/login';
-    };
+  const handleLoginClick = () => {
+    trackEvent("Button", "Click", "Hero Login Button Click");
+    // Navigate to the href
+    window.location.href = "/login";
+  };
 
-    const handleAlphaaiClick = () => {
-        trackEvent('Button', 'Click', 'Hero AlphaAI Button Click');
-        // Navigate to the href
-        window.location.href = '/chat';
-    };
-    
-    return (
-        <Container maxWidth="xl" style={{ padding: '40px 0', textAlign: 'center', position: 'relative' }}>
-            <AnimatedAlpha style={{ position: 'absolute', top: '10px', left: '10px', zIndex: 0 }} />
-            <Typography variant="h2" component="h1" style={{ color: theme.palette.text.primary }}gutterBottom>
-                Welcome to AlphaCharter
-            </Typography>
-            <Typography variant="h5" component="p" gutterBottom sx={{ margin: 5}}>
-                Your on-demand quantitative researcher.
-            </Typography>
-            <Button variant="contained" color="primary" onClick={handleLoginClick}>
-                Log In / Sign Up
-            </Button>
-            <Button variant='contained' color='secondary' sx={{ textTransform:'none', marginLeft: 5 }} onClick={handleAlphaaiClick}>
+  // const handleAlphaaiClick = () => {
+  //     trackEvent('Button', 'Click', 'Hero AlphaAI Button Click');
+  //     // Navigate to the href
+  //     window.location.href = '/chat';
+  // };
+
+  return (
+    <Container
+      maxWidth="xl"
+      style={{ padding: "40px 0", textAlign: "center", position: "relative" }}
+    >
+      <AnimatedAlpha
+        style={{ position: "absolute", top: "10px", left: "10px", zIndex: 0 }}
+      />
+      <Typography
+        variant="h2"
+        component="h1"
+        style={{ color: theme.palette.text.primary }}
+        gutterBottom
+      >
+        Welcome to AlphaCharter
+      </Typography>
+      <Typography variant="h5" component="p" gutterBottom sx={{ margin: 5 }}>
+        Your on-demand quantitative researcher.
+      </Typography>
+      <Button variant="contained" color="primary" onClick={handleLoginClick}>
+        Get Started
+      </Button>
+      {/* <Button variant='contained' color='secondary' sx={{ textTransform:'none', marginLeft: 5 }} onClick={handleAlphaaiClick}>
                 GO TO &nbsp; <strong>AlphaAI</strong>
-            </Button>
-        </Container>
-    );
+            </Button> */}
+    </Container>
+  );
 }
 
 export default HeroSection;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import HeroSection from './Herosection';
-import FeatureCards from './FeatureCards';
-import AppDemo from './AppDemo';
-import CallToAction from './CallToAction';
-import TestimonialsSection from './TestimonialSection';
+import React from "react";
+import HeroSection from "./Herosection";
+import FeatureCards from "./FeatureCards";
+import AppDemo from "./AppDemo";
+import CallToAction from "./CallToAction";
+import TestimonialsSection from "./TestimonialSection";
 
 function HomePage() {
-
   return (
     <div>
       <HeroSection />
       <FeatureCards />
       <AppDemo />
-      <CallToAction text='Make an account' href='/login' />
+      <CallToAction text="Make an account" href="/login" />
       <TestimonialsSection />
-
     </div>
   );
 }
